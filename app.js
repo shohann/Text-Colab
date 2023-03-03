13ffe7fd842782255614d38243a6933ada84e454
shohann <shohanurr490@gmail.com>
2023-03-03 13:53:26 +0600

    websocket added

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const WebSocket = require('ws');
 const app = express();
 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
-app.listen(3001, () => {
+const server = app.listen(3001, () => {
     console.log('Listening on port 3001');
+});
+
+const wss =  new WebSocket.Server({
+    server: server
+});
+
+wss.on('connection', function(ws) {
+    ws.on('message', function(data) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data.toString())
+            }
+        })
+    })
 })
+
+
+// app.listen(3001, () => {
+//     console.log('Listening on port 3001');
+// })
