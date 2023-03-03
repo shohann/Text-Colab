18ad7be21b46a4ae25014b2a92e041a5f8e01c6d
shohann <shohanurr490@gmail.com>
2023-03-03 15:27:09 +0600

    websocket added

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+let text = '';
+
 const server = app.listen(3001, () => {
     console.log('Listening on port 3001');
 });
@@ -24,6 +26,8 @@ wss.on('connection', function(ws) {
     ws.on('message', function(data) {
         wss.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
+                text = text.concat(data.toString())
+                console.log(text);
                 client.send(data.toString())
             }
         })
