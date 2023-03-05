5a9e966e0ce42df080dbfd35a4f5c11e01aee3d3
shohann <shohanurr490@gmail.com>
2023-03-05 14:54:17 +0600

    websocket added

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const wss =  new WebSocket.Server({
     server: server
 });
 
-
 const maxClients = 5;
 let rooms = {};
 
@@ -34,94 +33,103 @@ function genKey(length) {
     return result;
 }
 
+function create(params, ws) {
+  // console.log(params);
+  const room = genKey(5);
+  console.log(`roomID: ${room}`);
+  rooms[room] = [ws];
+  ws["room"] = room;
+  
+  
+  // console.log(rooms);
+  // console.log(wss.client.room);
+  generalInformation(ws);
+}
 
-// console.log(rooms);
+function join(params, ws) {
+  // console.log(params);
+  const room = params.code;
+  // console.log(room);
+  // console.log(rooms);
+  if (!Object.keys(rooms).includes(room)) {
+    console.warn(`Room ${room} does not exist!`);
+    return;
+  }
+
+  if (rooms[room].length >= maxClients) {
+    console.warn(`Room ${room} is full!`);
+    return;
+  }
+
+  rooms[room].push(ws);
+  // console.log(`Length: after join`);
+  // console.log(rooms);
+  // console.log('Length end');
+  ws["room"] = room;
+  
+  console.log(rooms);
+
+  generalInformation(ws);
+}
+
+
+function leave(params, ws) {
+  console.log(params);
+}
+
+function generalInformation(ws) {
+  // console.log('GN');
+  // console.log(rooms);
+  let obj;
+  if (ws["room"] === undefined){
+      obj = {
+          "type": "info",
+          "params": {
+            "room": ws["room"],
+            "no-clients": rooms[ws["room"]].length,
+          }
+        }
+  } else {
+      obj = {
+          "type": "info",
+          "params": {
+            "room": "no room",
+          }
+        }
+  }
+  ws.send(JSON.stringify(obj));
+}
+
 wss.on('connection', function(ws) {
-    
+  console.log(wss.clients.size);
     ws.on('message', function(data) {
 
-        ////
         const obj = JSON.parse(data.toString());
         const type = obj.type;
         const params = obj.params;
 
-        function create(params) {
-            // console.log(params);
-            const room = genKey(5);
-            rooms[room] = [ws];
-            ws["room"] = room;
-            
-            console.log(rooms);
-            generalInformation(ws);
+        if (type === "create") {
+            console.log('create');
+            create(params, ws)
+        } else if (type === "join") {
+            console.log('joined');
+            join(params, ws);
+        } else {
+              console.log('invalid');
+              leave(params, ws) // why?
         }
 
-        function join(params) {
-            // console.log(params);
-            const room = params;
-            console.log(room);
-            if (!Object.keys(rooms).includes(room)) {
-              console.warn(`Room ${room} does not exist!`);
-              return;
-            }
-          
-            if (rooms[room].length >= maxClients) {
-              console.warn(`Room ${room} is full!`);
-              return;
-            }
-          
-            rooms[room].push(ws);
-            ws["room"] = room;
-            
-            console.log(rooms.room);
+        // console.log(wss.clients.size);
+        // wss.clients.forEach(function each(client) {
+        //     // broadcast
+        //     // filter option -> has
+        //     // && client !== ws
+        //     if (client.readyState === WebSocket.OPEN ) {
+        //         // console.log(data.toString());
+        //         client.send('1'); // echo back
 
-            generalInformation(ws);
-        }
-
-
-        function leave(params) {
-            console.log(params);
-        }
-
-        function generalInformation(ws) {
-            let obj;
-            if (ws["room"] === undefined){
-                obj = {
-                    "type": "info",
-                    "params": {
-                      "room": ws["room"],
-                      "no-clients": rooms[ws["room"]].length,
-                    }
-                  }
-            } else {
-                obj = {
-                    "type": "info",
-                    "params": {
-                      "room": "no room",
-                    }
-                  }
-            }
-            ws.send(JSON.stringify(obj));
-          }
-        ////
-    
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                // client.send(data.toString()); // echo back
-
-                if (type === "create") {
-                    // console.log('create');
-                    create(params)
-                } else if (type === "join") {
-                    // console.log('joined');
-                    join(params);
-                } else {
-                    console.log('invalid');
-                    leave(params) // why?
-                }
-
-                // client.send(JSON.stringify(obj));
-            }
-        });
+        //     }
+        // });
 
 
     });
